871694d69c4959b305ff03d6344ceae9b43e16ba
escrip <escrip@artic.edu>
2021-03-02 15:35:39 -0600

    particles done (good enuf)

--- sketches/waterfallMirror/sketch.js
+++ sketches/waterfallMirror/sketch.js
@@ -20,6 +20,8 @@ function draw() {
     p.draw();
     p.update();
   }
+  let p = new Particle();
+  particles.push(p);
 }
 
 class Particle {
@@ -30,41 +32,41 @@ class Particle {
     this.pos = createVector(random(width), 1);
     this.vel = createVector(random(-1, 1), random(-1, 1));
     //this.vel = p5.Vector.random2D().mult(random(0,2));
-    let randomExp = random(1, 10);
-    this.acc = createVector(pow(random(-0.03, 0.03), randomExp), pow(random(-0.03, 0.03), randomExp));
+    //let randomExp = random(1, 10);
+    this.acc = createVector(0, .01);
     this.color = color(random(255), random(255), random(255));
     this.size = 5;
-    this.drag = .999;
+    //this.drag = .999;
   }
   
-//   checkWalls() {
-//     // check walls, make particles bounce off walls
-//     if(this.pos.y > (height - this.size)) {
-//       this.vel.y *= -1;
-//       this.pos.y = height-this.size;
-//     }
+  checkWalls() {
+    // check walls, make particles bounce off walls
+    // if(this.pos.y > (height - this.size)) {
+    //   this.vel.y *= -1;
+    //   this.pos.y = height-this.size;
+    // }
     
-//     if(this.pos.y < 0) {
-//       this.vel.y *= -1;
-//       this.pos.y = 0;
-//     }
+    // if(this.pos.y < 0) {
+    //   this.vel.y *= -1;
+    //   this.pos.y = 0;
+    // }
     
-//     if(this.pos.x > width - this.size) {
-//       this.vel.x *= -1;
-//       this.pos.x = width-this.size;
-//     }
+    if(this.pos.x > width - this.size) {
+      this.vel.x *= -1;
+      this.pos.x = width-this.size;
+    }
     
-//     if(this.pos.x < 0) {
-//       this.vel.x *= -1;
-//       this.pos.x = 0;
-//     }
-//   }
+    if(this.pos.x < 0) {
+      this.vel.x *= -1;
+      this.pos.x = 0;
+    }
+  }
   
   update() {
     this.vel.add(this.acc);
     //this.vel.mult(this.drag);
     this.pos.add(this.vel);
-    //his.checkWalls();
+    this.checkWalls();
     
     
     // loop particle back to top
