28d216a11604f27e45d4b7bd377b67543260f457
escrip <escrip@artic.edu>
2021-03-02 14:11:36 -0600

    added particles
    doesnt interact with webcam

--- sketches/waterfallMirror/sketch.js
+++ sketches/waterfallMirror/sketch.js
@@ -2,7 +2,6 @@ let w = 320;
 let h = 180;
 let capture;
 
-let particle;
 let particles = [];
 
 function setup() {
@@ -12,8 +11,13 @@ function setup() {
     capture = createCapture(VIDEO);
     capture.size(w, h);
     capture.hide();
-    
+
     rectMode(CENTER);
+
+    for(let i = 0; i < 50; i++){
+        let p = new Particle();
+        particles.push(p);
+    }
   }
 
 function draw() {
@@ -22,7 +26,7 @@ function draw() {
 
     for(let y = 0; y < capture.height; y++){
         for(let x = 0; x < capture.width; x++){
-            const index = (x + y * capture.width) * 4;
+            const index = (x + y * capture.width) * 4; // get x + y of pixel
 
             let r = capture.pixels[index];
             let g = capture.pixels[index+1];
@@ -38,7 +42,74 @@ function draw() {
             pop();
         }
     }
-    capture.updatePixels();
+    //capture.updatePixels();
 
     image(capture, 0, 0);
+
+    for(let i; i < particles.length; i++){
+        let p = particles[i];
+
+        p.draw();
+        p.update();
+    }
 }
+
+class Particle {
+  
+    constructor() {
+      // this.x = random(width);
+      // this.y = random(height);
+      this.pos = createVector(width/2, height/2);
+      this.vel = createVector(random(-1, 1), random(-1, 1));
+      //this.vel = p5.Vector.random2D().mult(random(0,2));
+      let randomExp = random(1, 10);
+      this.acc = createVector(pow(random(-0.03, 0.03), randomExp), pow(random(-0.03, 0.03), randomExp));
+      this.color = color(random(255), random(255), random(255));
+      this.size = random(2, 4);
+      this.drag = .999;
+    }
+    
+    checkWalls() {
+      // check walls, make particles bounce off walls
+      if(this.pos.y > (height - this.size)) {
+        this.vel.y *= -1;
+        this.pos.y = height-this.size;
+      }
+      
+      if(this.pos.y < 0) {
+        this.vel.y *= -1;
+        this.pos.y = 0;
+      }
+      
+      if(this.pos.x > width - this.size) {
+        this.vel.x *= -1;
+        this.pos.x = width-this.size;
+      }
+      
+      if(this.pos.x < 0) {
+        this.vel.x *= -1;
+        this.pos.x = 0;
+      }
+    }
+    
+    update() {
+      this.vel.add(this.acc);
+      //this.vel.mult(this.drag);
+      this.pos.add(this.vel);
+      this.checkWalls();
+      
+      
+      // loop particle back to top
+      // if(this.pos.y > height) {
+      //   this.pos.y = 0;
+      //   this.vel = createVector(random(-1, 1), random(-1, 1));
+      // }
+    }
+    
+    draw() {
+      fill(this.color);
+      //tint(255, 255, 0);
+      // image(img, this.pos.x, this.pos.y, this.size, this.size);
+      ellipse(this.pos.x, this.pos.y, this.size, this.size);
+    }
+  }
